7beeedfa76d691e0001b2623d4f9c8e19d4df60e
Gokruzk <nigell@gmail.com>
2024-05-04 21:18:52 -0500

    feat: upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, Response
+from bson import json_util, ObjectId
+from email.message import EmailMessage
+import smtplib
+
+
+app = Flask(__name__)
+
+
+def send_message(desde, para):
+    # Welcome message
+    msg = f'Welcome {para}'
+
+    # email instance
+    email = EmailMessage()
+
+    # email config
+    email['From'] = desde
+    email['To'] = para
+    email['Subject'] = 'Welcome'
+    email.set_content(msg)
+
+    smtp = smtplib.SMTP_SSL('smtp.gmail.com')
+    smtp.login(desde, 'ltbj rfru hmrz hwpi')
+    smtp.sendmail(desde, para, email.as_string())
+    smtp.quit()
+
+
+@app.route('/email', methods=['POST'])
+def send_mail():
+    # retreiving email
+    datos = request.json
+    desde = 'nigelljama@gmail.com'
+    para = datos['email']
+    # sending email
+    send_message(desde, para)
+    return {'message': 'sent'}
+
+
+if __name__ == "__main__":
+    app.run(port=5002, debug=True)
